26a31f67ed1c6ddf220002fe64992c4a1d361379
casz92 <suago@hotmail.com>
2025-06-19 11:04:49 -0400

    package

--- mix.exs
+++ mix.exs
@@ -1,29 +1,32 @@
 defmodule Poolder.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :poolder,
-      description: "A compile-time builder that generates a concurrent pool of worker processes for parallel task execution",
-      version: "0.1.0",
+      name: "Poolder",
+      description:
+        "A compile-time builder that generates a concurrent pool of worker processes for parallel task execution",
+      source_url: "https://github.com/casz92/poolder",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      packages: packages()
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
-  def application do
-    []
-  end
+  def application, do: []
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     []
   end
 
-  defp packages do
+  defp package do
     [
       maintainers: ["Carlos Suarez"],
       files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
